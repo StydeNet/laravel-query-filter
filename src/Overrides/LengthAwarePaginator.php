dea577e82d44c90f764fccd57cbf50a669ace3de
Luis Arce <luilliarcec@gmail.com>
2020-12-12 12:26:32 -0500

    Implementing the contract in LengthAwarePaginator

--- src/Overrides/LengthAwarePaginator.php
+++ src/Overrides/LengthAwarePaginator.php
@@ -3,9 +3,10 @@
 namespace Styde\QueryFilter\Overrides;
 
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
+use \Styde\QueryFilter\Contracts\Sortable as SortableContract;
 use Styde\QueryFilter\Concerns\Sortable;
 
-class LengthAwarePaginator extends Paginator
+class LengthAwarePaginator extends Paginator implements SortableContract
 {
     use Sortable;
 }
